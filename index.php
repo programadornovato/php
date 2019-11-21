7c5c33fe27b45dd207691ea07f246e6b104f4101
Programador Novato <info@programadornovato.com>
2019-11-20 23:10:35 -0600

    Curso de PHP y MySql [04.- Comentarios, Variables, echo y print]
    Video:
    https://youtu.be/Hqkn8BeXFJg

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-    echo "Hola mundo desde index.php";
+    /*
+    Author: eugenio
+    Author page: programadornovato.com
+    Date:
+    */
+
+    //Con este codigo se muestra en el navegado un saludo
+    //echo "Hola mundo desde index.php";
+
+    /*
+    Esto es un comentario
+    Esto tambien 
+    y aqui hay mas cometarios
+    */
+    /*
+    $paginaWeb="programadornovato.com";
+    $paginaWeb=55;
+    $paginaWeb=$paginaWeb+2;
+    echo $paginaWeb;
+    */
+    $nombre="eugenio";
+    echo 'Humano tu nombre es ',$nombre,5,1,"</br>";
+
+    print 'Humano tu nombre es '.$nombre;
+
+
 ?>
