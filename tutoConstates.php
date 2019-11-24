f9a8279e7b196b889080af8aaf6b61d7decf30af
Programador Novato <info@programadornovato.com>
2019-11-24 03:42:05 -0600

    Curso de PHP y MySql [13.- Que es una constante y como definirla]
    Video:
    https://youtu.be/wYRihnBFDJ4

---
+++ tutoConstates.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Constantes</title>
+</head>
+<body>
+    <?php
+    /*
+    define("SALIR","Nel no sales");
+    echo SALIR;
+    define("SALIR","Que si salgo y hazle como quieras");
+    
+    define("SALIR","Nel no sales",true);
+    echo salir;
+    */
+    echo "A ver php adivina en que linea estoy:".__LINE__."</br>";
+    echo "A ver php en que archivo estoy: ".__FILE__."</br>";
+    echo "A ver php en que directorio estoy: ".__DIR__;
+    ?>
+</body>
+</html>
