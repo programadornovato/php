5f24146d20406935ca5029aa0f58fecaa84814d8
Programador Novato <info@programadornovato.com>
2019-11-26 22:51:06 -0600

    Curso de PHP y MySql [16.- Operadores matemáticos (Parte 3)]
    Video:
    https://youtu.be/2Hs5q9u8A9Y

--- lib.php
+++ lib.php
@@ -5,5 +5,26 @@
     function cambioAlcance(){
         $GLOBALS['chancla']="No, no me alcanzaste</br>";
     }
-
+    function operacionesMat($operador='',$num1=0,$num2=0)
+    {
+        if ($operador == 'suma') {
+            echo 'El resultado de la operacion es: ' . ($num1 + $num2);
+        }
+        if ($operador == 'resta') {
+            echo 'El resultado de la operacion es: ' . ($num1 - $num2);
+        }
+        if ($operador == 'multiplicacion') {
+            echo 'El resultado de la operacion es: ' . ($num1 * $num2);
+        }
+        if ($operador == 'divicion') {
+            echo 'El resultado de la operacion es: ' . ($num1 / $num2);
+        }
+        if ($operador == 'modulo') {
+            echo 'El resultado de la operacion es: ' . ($num1 % $num2);
+        }
+        if ($operador == '') {
+            echo 'Humano, debes de seleccionar un operador matematico ';
+        }
+    }
+    
 ?>
--- operodoresMatematicos.php
+++ operodoresMatematicos.php
@@ -8,6 +8,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tutorial de operadores matematicos</title>
 </head>
+<?php
+require_once "lib.php";
+?>
 
 <body>
     <div class="container mt-2">
@@ -16,22 +19,22 @@
                 <form>
                     <div class="form-group">
                         <label>Numero 1</label>
-                        <input type="text" class="form-control" name="num1" value="<?php echo $_GET['num1']??0; ?>">
+                        <input type="text" class="form-control" name="num1" value="<?php echo $_GET['num1'] ?? 0; ?>">
                     </div>
                     <div class="form-group">
                         <label>Numero 2</label>
-                        <input type="text" class="form-control" name="num2" value="<?php echo $_GET['num2']??0; ?>">
+                        <input type="text" class="form-control" name="num2" value="<?php echo $_GET['num2'] ?? 0; ?>">
                     </div>
                     <div class="form-group">
                         <label>Operador</label>
-                        <?php $operador=$_GET['operador']??''; ?>
+                        <?php $operador = $_GET['operador'] ?? ''; ?>
                         <select name="operador" class="form-control">
-                            <option value="" <?php if($operador=='')echo "selected='selected'"; ?> >Seleccione un operador</option>
-                            <option value="suma"<?php if($operador=='suma')echo "selected='selected'"; ?>>Suma</option>
-                            <option value="resta"<?php if($operador=='resta')echo "selected='selected'"; ?>>Resta</option>
-                            <option value="multiplicacion"<?php if($operador=='multiplicacion')echo "selected='selected'"; ?>>Multiplicacion</option>
-                            <option value="divicion"<?php if($operador=='divicion')echo "selected='selected'"; ?>>Divicion</option>
-                            <option value="modulo"<?php if($operador=='modulo')echo "selected='selected'"; ?>>Modulo</option>
+                            <option value="" <?php if ($operador == '') echo "selected='selected'"; ?>>Seleccione un operador</option>
+                            <option value="suma" <?php if ($operador == 'suma') echo "selected='selected'"; ?>>Suma</option>
+                            <option value="resta" <?php if ($operador == 'resta') echo "selected='selected'"; ?>>Resta</option>
+                            <option value="multiplicacion" <?php if ($operador == 'multiplicacion') echo "selected='selected'"; ?>>Multiplicacion</option>
+                            <option value="divicion" <?php if ($operador == 'divicion') echo "selected='selected'"; ?>>Divicion</option>
+                            <option value="modulo" <?php if ($operador == 'modulo') echo "selected='selected'"; ?>>Modulo</option>
                         </select>
                     </div>
                     <button type="submit" name="enviar" value="enviar" class="btn btn-primary">Enviar</button>
@@ -42,31 +45,14 @@
             <div class="col-12">
                 <div class="alert alert-primary" role="alert">
                     <?php
-                    $botonEnviar=$_GET['enviar']??'';
-                    if($botonEnviar=='enviar'){
-                        $num1=$_GET['num1']??0;
-                        $num2=$_GET['num2']??0;
-                        if($operador=='suma'){
-                            echo 'El resultado de la operacion es: '.($num1+$num2);
-                        }
-                        if($operador=='resta'){
-                            echo 'El resultado de la operacion es: '.($num1-$num2);
-                        }
-                        if($operador=='multiplicacion'){
-                            echo 'El resultado de la operacion es: '.($num1*$num2);
-                        }
-                        if($operador=='divicion'){
-                            echo 'El resultado de la operacion es: '.($num1/$num2);
-                        }
-                        if($operador=='modulo'){
-                            echo 'El resultado de la operacion es: '.($num1%$num2);
-                        }
-                        if($operador==''){
-                            echo 'Humano, debes de seleccionar un operador matematico ';
-                        }
+                    $botonEnviar = $_GET['enviar'] ?? '';
+                    if ($botonEnviar == 'enviar') {
+                        $num1 = $_GET['num1'] ?? 0;
+                        $num2 = $_GET['num2'] ?? 0;
+                        operacionesMat($operador,$num1,$num2);
                     }
                     ?>
-                    
+
                 </div>
             </div>
         </div>
