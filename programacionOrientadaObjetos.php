bc57f94f0e8a2bcef31238ea2535a8e65122ab1a
Programador Novato <info@programadornovato.com>
2019-12-02 14:22:27 -0600

    Curso de PHP y MySql [27.- POO(Programacion Orientada a Objetos)]
    Video:
    https://youtu.be/J3ciylTAo2Q

---
+++ programacionOrientadaObjetos.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Programacion Orientada a Objetos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      <?php
+          class carro{
+
+          }
+          $lamborgini1=new carro();
+          $lamborgini2=new carro();
+          $bmw1=new carro();
+          $bmw2=new carro();
+          $cybertruck1=new carro();
+          $cybertruck2=new carro();
+          $cybertruck3=new carro();
+          $cybertruck4=new carro();
+      ?>
+  </body>
+</html>
