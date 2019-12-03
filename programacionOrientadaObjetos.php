91d4d1bac35c0752bdedcf8ff1bc0082f5dc7c41
Programador Novato <info@programadornovato.com>
2019-12-02 23:00:43 -0600

    Curso de PHP y MySql [28.- POO(Programación Orientada a Objetos (constrictor) parte 2)]
    Vdeo:
    https://youtu.be/Bt-LEsTkEXw

--- programacionOrientadaObjetos.php
+++ programacionOrientadaObjetos.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Programacion Orientada a Objetos</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,20 +9,34 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      <?php
-          class carro{
+</head>
 
-          }
-          $lamborgini1=new carro();
-          $lamborgini2=new carro();
-          $bmw1=new carro();
-          $bmw2=new carro();
-          $cybertruck1=new carro();
-          $cybertruck2=new carro();
-          $cybertruck3=new carro();
-          $cybertruck4=new carro();
-      ?>
-  </body>
+<body>
+    <?php
+    class carro
+    {
+        function __construct($marca,$modelo,$alto=1.5,$largo=3,$color="Blanco")
+        {
+            $this->marca=$marca;
+            $this->modelo=$modelo;
+            $this->alto=$alto;
+            $this->largo=$largo;
+            $this->color=$color;
+        }
+        var $marca;
+        var $modelo;
+        var $alto;
+        var $largo;
+        var $color;
+        var $velocidad;
+        var $angulo;
+        var $imagen;
+    }
+    $lamborgini1 = new carro("Lamborgini",2020);
+    $bmw1 = new carro("BMW",2020,1.3,4,"Rojo");
+    $cybertruck1 = new carro("Tesla","Cyber truck 2021");
+    echo "color del lambo=".$lamborgini1->color;
+    ?>
+</body>
+
 </html>
