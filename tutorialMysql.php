0e9aa5f6a095b1c6b4ada66cc64817d7074eead9
Programador Novato <info@programadornovato.com>
2019-12-11 03:41:24 -0600

    Curso de PHP y MySql [44.- Leer todos los registros de nuestra tabla de MySQL ]
    Video:
    https://youtu.be/rxU1xdIRVKo

--- tutorialMysql.php
+++ tutorialMysql.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Tutorial Mysql</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,25 +9,33 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      <?php
-          $db_host="localhost";
-          $db_user="eugenio";
-          $db_pass="123456";
-          $db_database="empresa";
-          $conexion=mysqli_connect($db_host,$db_user,$db_pass,$db_database);
-          //var_dump($conexion);
-          if($conexion==false){
-              die("Error de conexion");
-          }
-          $sql = "SELECT `marca`, `modelo`, `anio` FROM `autos`";
-          $resultSet=mysqli_query($conexion,$sql);
-          $row=mysqli_fetch_row($resultSet);
-          var_dump($row);
-          echo $row[0]."<br>";
-          echo $row[1]."<br>";
-          echo $row[2]."<br>";
-          ?>
-  </body>
+</head>
+
+<body>
+    <?php
+    ini_set("display_errors",1);
+    ini_set("display_startup_errors",1);
+    $db_host = "localhost";
+    $db_user = "eugenio";
+    $db_pass = "123456";
+    $db_database = "empresa";
+    $conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+    if ($conexion == false) {
+        die(
+            "Error de conexion a Mysql con el codigo: " . mysqli_connect_errno().
+            "<br>".mysqli_connect_error()
+        );
+    }
+    $sql = "SELECT `marca`, `modelo`, `anio` FROM `autos`";
+    $resultSet = mysqli_query($conexion, $sql);
+    while( $row = mysqli_fetch_row($resultSet) ){
+        
+        echo $row[0] . " | ";
+        echo $row[1] . " | ";
+        echo $row[2] . " | <br>";
+    }
+    mysqli_close($conexion);
+    ?>
+</body>
+
 </html>
