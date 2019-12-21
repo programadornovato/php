3e7ed4d84ab4884bf1af0cecbb85d21263433b5e
Programador Novato <info@programadornovato.com>
2019-12-21 07:58:32 -0600

    Curso de PHP🐘 y MySql🐬 [66.- Consultas preparadas]
    Video:
    https://youtu.be/gcb-iuXNIsg

--- consultasPreparadas.php
+++ consultasPreparadas.php
@@ -24,26 +24,49 @@
                         <button type="submit" class="btn btn-secondary" name="buscar" value="buscar">Buscar</button>
                     </div>
                 </form>
-                <table class="table table-striped table-inverse table-responsive">
-                    <thead class="thead-inverse">
-                        <tr>
-                            <th>id</th>
-                            <th>nombre</th>
-                            <th>precioCompra</th>
-                            <th>categoria</th>
-                            <th>unidadesEnExistencia</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <?php
+                include_once "db_empresa.php";
+                $conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+                $query = "SELECT id,nombre,precioCompra,categoria,unidadesEnExistencia FROM productos
+                    WHERE unidadesEnExistencia=?;";
+                $sentencia = mysqli_prepare($conexion, $query);
+                $existencia = $_REQUEST['existencia'] ?? 0;
+                mysqli_stmt_bind_param($sentencia, "i", $existencia);
+                $resultado = mysqli_stmt_execute($sentencia);
+                if ($resultado == true) {
+                ?>
+                    <table class="table table-striped table-inverse">
+                        <thead class="thead-inverse">
+                            <tr>
+                                <th>id</th>
+                                <th>nombre</th>
+                                <th>precioCompra</th>
+                                <th>categoria</th>
+                                <th>unidadesEnExistencia</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            mysqli_stmt_bind_result($sentencia,$id, $nombre, $precioCompra, $categoria, $unidadesEnExistencia);
+                            while (mysqli_stmt_fetch($sentencia)) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $id; ?></td>
+                                    <td><?php echo $nombre; ?></td>
+                                    <td><?php echo $precioCompra; ?></td>
+                                    <td><?php echo $categoria; ?></td>
+                                    <td><?php echo $unidadesEnExistencia; ?></td>
+                                </tr>
+
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
